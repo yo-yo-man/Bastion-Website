13435f674bea4c9111503d0080e6f3548cbec6ec
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-06-01 16:42:48 +0000

    components: add support for null links in ExternalLink

--- src/components/ExternalLink.js
+++ src/components/ExternalLink.js
@@ -3,9 +3,13 @@ import React from 'react';
 class ExternalLink extends React.Component {
   render() {
     return (
-      <a href={ this.props.to } target='_blank'>
-        { this.props.children }
-      </a>
+      this.props.to === '#'
+      ? <a href='javascript:void(0);'>
+          { this.props.children }
+        </a>
+      : <a href={ this.props.to } target='_blank'>
+          { this.props.children }
+        </a>
     );
   }
 }
